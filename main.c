048ed9b2a21d46b78d15ca7d5f0b9678cbfa6b9f
Ivan <ivaz3823@gmail.com>
2018-12-26 12:45:49 +0300

    add task

--- main.c
+++ main.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
-#include "preferences.h"
-#include "source.h"
+#include <string.h>
 
+struct coord{           //Структура
+  int x;
+  int y;
+};
+
+
+struct coord* shiftcoords(struct coord* coord1, int Xshift, int Yshift);
+//Функция по сдвигу координат в экземпляре структуры, которая возвращает тот же самый адрес
+////Потому что зачем возвращать указатель на структуру без массива и без динамической памяти
 int main()
 {
-    unsigned int size = 5;
-    unsigned int user;
-    struct preferences array[size];
-    scaner(array, size);
-    printf("Enter user: ");
-    scanf("%u", &user);
-    printf("\n\n");
-    analizer(array, size, user);
+    struct coord A;     //ОбЪявляем экземпляр мтуктуры типа coord
+    A.x = 3;            //Заполняем его
+    A.y = -9;
+    struct coord B = *shiftcoords(&A, -7, 10);    //Сдвигаем х на -7 и у на 10
+    // Присваиваем разыменованное значение к экземпляру В структуры coord значение экземпляра А
+    printf("%d",B.x);       //Выводим
+    printf("\t");
+    printf("%d",B.y);
+    printf("\n");
     return 0;
+} // Ответ: -4 и 1
+
+
+struct coord* shiftcoords(struct coord* coord1, int Xshift, int Yshift)
+{
+
+    coord1->x += Xshift;    //Добавляем к полям значения
+    coord1->y += Yshift;
+    return coord1;          //Зачем-то возвращаем тот же адрес
 }
